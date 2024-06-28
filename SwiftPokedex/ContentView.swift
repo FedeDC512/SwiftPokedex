40eb921ebbc3f70122631d127f359897b8900c34
FedeDC512 <78120567+FedeDC512@users.noreply.github.com>
2024-06-28 16:33:10 +0200

    Initial Commit

---
+++ SwiftPokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPokedex
+//
+//  Created by Federico Agnello on 28/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPokedex/SwiftPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPokedexApp.swift
+//  SwiftPokedex
+//
+//  Created by Federico Agnello on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
